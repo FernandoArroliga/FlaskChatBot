1679b35611912ee533ea2256c4716a6f4348a50d
Fernando Arroliga <FernandoArroliga@hotmail.com>
2023-09-30 20:28:46 -0600

    "Add basic chatbot configuration"

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
 from flask import Flask, render_template, request, jsonify
 
+# ChatBot modules and libraries
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+# ChatBot Basic Configuration
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+# Defining Basic Flask App
 app = Flask(__name__)
 
+# Defining flask Routes
 @app.route("/")
 def index():
     return render_template("chat.html")
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    
+    msg = request.form["msg"]
+    input = msg
+    
+    return get_Chat_response(input)
+
+def get_Chat_response(text):
+        
+    # Let's chat for 5 lines
+    for step in range(5):
+        # encode the new user input, add the eos_token and return a tensor in Pytorch
+        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+
+        # append the new user input tokens to the chat history
+        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+
+        # generated a response while limiting the total chat history to 1000 tokens, 
+        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+        # pretty print last ouput tokens from bot
+        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+
+if __name__ == "__main__":
+    app.run()
